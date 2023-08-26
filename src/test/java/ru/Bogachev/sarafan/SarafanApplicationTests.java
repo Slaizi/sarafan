82dc5b68f289a2c9fec3d6f1b1bbf7e20ddf91d3
Slaizi <bogachev_konstantin@bk.ru>
2023-08-26 07:26:52 +0300

    Initial commit

---
+++ src/test/java/ru/Bogachev/sarafan/SarafanApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.Bogachev.sarafan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SarafanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
